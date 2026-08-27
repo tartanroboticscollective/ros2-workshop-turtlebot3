279d1b72aa23fdc614505ec67356902b3cda3442
Alejandro Bordallo <alex.bordallo@ed.ac.uk>
2026-08-27 18:35:15 +0100

    Ensure docker always pulls latest available images

--- docker/run.sh
+++ docker/run.sh
@@ -55,7 +55,7 @@ done
 xhost + >/dev/null
 
 # Run docker image with local code volumes for development
-docker run -it --rm --net host --privileged \
+docker run --pull=always -it --rm --net host --privileged \
     --name ros2-workshop-turtlebot3 \
     -e DISPLAY="$DISPLAY" -v /tmp/.X11-unix/:/tmp/.X11-unix \
     -e QT_X11_NO_MITSHM=1 \
